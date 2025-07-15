d4edda69bd8c8d21ffc0286592a0a0945770ee8f
krunalp1908 <krunalp1908@gmail.com>
2025-07-16 00:12:01 +0530

    file added 3

--- demo.py
+++ demo.py
@@ -1,6 +1,8 @@
-def myFunction():
+"""This module returns a greeting message."""
+
+def my_function():
+    """Return a greeting string."""
     return "Hello dosto"
 
-
 if __name__ == "__main__":
-    myFunction()
+    my_function()
